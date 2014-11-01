96dec70426ccbab959f500e4fdb8ae7eb2083b99
Cichy3D <wojciech.tomczyk2@gmail.com>
2014-11-01 15:38:41 +0100

    ustawienia projektu

---
+++ src/pl/passus/app/view/Hello.java
@@ -0,0 +1,37 @@
+package pl.passus.app.view;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet({ "/Hello", "/hello" })
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Hello!");
+		System.out.println("Buu");
+		
+		
+	}
+
+}
